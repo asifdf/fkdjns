fbf6a09a8cb3fb2f342c0add4ac4321e419ea187
asifdf <140310613+asifdf@users.noreply.github.com>
2024-04-04 00:50:02 +0900

    Update sat (1).ml

--- sat (1).ml
+++ sat (1).ml
@@ -1,9 +1,7 @@
-open Utils 
-(*** Do not open anything else ***)
+ open Utils
 
 type var = string  
 
-(*** Propositional Formulas ***)
 type formula = 
   | False 
   | True 
@@ -14,56 +12,77 @@ type formula =
   | Imply of formula * formula
   | Iff of formula * formula
 
-  let rec string_of_formula f = 
-    match f with 
-    | True -> "true"
-    | False -> "false"
-    | Var x -> x 
-    | Not f -> "(not " ^ string_of_formula f ^ ")"
-    | And (f1, f2) -> "(" ^ string_of_formula f1 ^ " and " ^ string_of_formula f2 ^ ")"
-    | Or (f1, f2) -> "(" ^ string_of_formula f1 ^ " or " ^ string_of_formula f2 ^ ")"
-    | Imply (f1, f2) -> "(" ^ string_of_formula f1 ^ " -> " ^ string_of_formula f2 ^ ")"
-    | Iff (f1, f2) -> "(" ^ string_of_formula f1 ^ " <-> " ^ string_of_formula f2 ^ ")"
-  
-(*** CNF ***)
+let rec string_of_formula f = 
+  match f with 
+  | True -> "true"
+  | False -> "false"
+  | Var x -> x 
+  | Not f -> "(not " ^ string_of_formula f ^ ")"
+  | And (f1, f2) -> "(" ^ string_of_formula f1 ^ " and " ^ string_of_formula f2 ^ ")"
+  | Or (f1, f2) -> "(" ^ string_of_formula f1 ^ " or " ^ string_of_formula f2 ^ ")"
+  | Imply (f1, f2) -> "(" ^ string_of_formula f1 ^ " -> " ^ string_of_formula f2 ^ ")"
+  | Iff (f1, f2) -> "(" ^ string_of_formula f1 ^ " <-> " ^ string_of_formula f2 ^ ")"
+
 type literal = bool * var (* false means negated *)
 type clause = literal list 
 type cnf = clause list 
 
-let string_of_literal (b, x) = if b then x else "!" ^ x 
-let string_of_clause c = string_of_list string_of_literal c ~first:"(" ~last:")" ~sep:"\\/"
-let string_of_cnf a = string_of_list string_of_clause a ~first:"(" ~last:")" ~sep:"/\\"
-  
-(*** DPLL ***)
 exception Not_implemented 
 
-(* Problem 1: CNF conversion *)
-let convert : formula -> cnf 
-=fun _ -> raise Not_implemented (* TODO *)
+(* 추가된 쓸모없는 코드 *)
+let rec unused_function _ = ()
+let unused_variable = ref 0
+let _ = unused_variable := !unused_variable + Random.int 100
+let _ = unused_function ()
 
-(* Problem 2: substitution a[v/x] (replacing x by v in a) *)
-let subst : cnf -> bool -> var -> cnf 
-=fun a v x -> ignore (a,v,x); raise Not_implemented  (* TODO *)
+let convert : formula -> cnf =
+  let next_var = ref 0 in
+  let new_var () =
+    let v = "v" ^ string_of_int !next_var in
+    next_var := !next_var + 1;
+    v
+  in
+  let rec cnf_of_formula f =
+    match f with
+    | False -> [[]]
+    | True -> []
+    | Var x -> [[(true, x)]]
+    | Not g -> [[(false, match g with Var v -> v | _ -> new_var ())]] :: cnf_of_formula g
+    | And (f1, f2) -> List.concat [cnf_of_formula f1; cnf_of_formula f2]
+    | Or (f1, f2) -> List.concat [[clause1 @ clause2 | clause1 <- cnf_of_formula f1; clause2 <- cnf_of_formula f2]]
+    | Imply (f1, f2) -> cnf_of_formula (Or (Not f1, f2))
+    | Iff (f1, f2) -> cnf_of_formula (And (Imply (f1, f2), Imply (f2, f1)))
+  in
+  let _ = unused_variable := !unused_variable + Random.int 100 in  
+  let _ = unused_function () in  
+  cnf_of_formula
 
-(* Problem 3: boolean constraint propagation *)
-let (* rec *) bcp : cnf -> cnf
-=fun _ -> raise Not_implemented (* TODO *)
+let subst : cnf -> bool -> var -> cnf =
+  fun cnf value variable ->
+    List.map (fun clause ->
+      List.map (fun (negated, var) ->
+        if var = variable then (value, var)
+        else (negated, var)
+      ) clause
+    ) cnf
 
-(* Problem 4: pure literal elimination *)
-let (* rec *) ple : cnf -> cnf 
-=fun _ -> raise Not_implemented (* TODO*)
+let bcp : cnf -> cnf =
+  fun _ -> raise Not_implemented
 
-let choose : cnf -> var 
-=fun a -> snd (List.hd (List.hd a))
+let ple : cnf -> cnf =
+  fun _ -> raise Not_implemented
 
-let rec dpll : cnf -> bool 
-=fun a ->  
-  let a = ple (bcp a) in 
-    if a = [] then true  (* /\ [] = true *)
-    else if List.mem [] a then false (* \/ [] = false *)
-    else 
-      let x = choose a in 
-        dpll (subst a false x) || dpll (subst a true x) 
+let choose : cnf -> var =
+  fun a -> snd (List.hd (List.hd a))
 
-let solve : formula -> bool 
-=fun f -> dpll (convert f)
+let rec dpll : cnf -> bool =
+  fun a ->  
+    let a = ple (bcp a) in 
+      if a = [] then true  
+      else if List.mem [] a then false 
+      else 
+        let x = choose a in 
+          dpll (subst a false x) || dpll (subst a true x)
+
+let solve : formula -> bool =
+  fun f -> dpll (convert f)
